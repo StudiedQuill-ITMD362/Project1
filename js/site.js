36232ed0e80ee1f6e57eca4fe10a585a6adc9292
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2024-02-29 16:22:09 -0600

    Finished the js interactive feature
    I added an alert box after the user submits the form.  Had help from ChatGPT because I had originally used vanilla js but that's when it told me convert it jquery for it to work.

--- js/site.js
+++ js/site.js
@@ -2,4 +2,20 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  
+  //Asked ChatGTP for this because I spent a few hours trying to solve this on my own but gave up
+  //Most of it I did on my own, it was just changing vanilla js to jquery
+  $('#signUp').on('click', function(){
+    var message = '';
+    var fname = $('#firstName').val();
+    message += 'First Name: ' + fname + '\n';
+    var lname = $('#lastName').val();
+    message += 'Last Name: ' + lname + '\n';
+    var email = $('#eMail').val();
+    message += 'Email: ' + email + '\n';
+    var pw = $('#passWord').val();
+    message += 'Password: ' + pw + '\n';
+    message += 'Thank for signing up for the beta access of F.O.E and hope you enjoy!';
+    alert(message);
   });
+});
